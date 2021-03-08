79630ec15ebefe69126f242716d786ba6967dfed
Junliang Yan <jyan@ca.ibm.ca>
2021-03-08 11:23:17 -0500

    Add sDateTime and eDateTime options

--- HistoricalDataDownloader.py
+++ HistoricalDataDownloader.py
@@ -11,7 +11,7 @@ from termcolor import colored
 import argparse
 import colorama
 '''
-json format for incoming traffic:
+raw json format for incoming traffic:
 {
     "Type": 803,
     "RequestID": 10,
@@ -28,17 +28,20 @@ json format for incoming traffic:
 }
 '''
 
-
 class Downloader:
 
-    def __init__(self, client, outfile):
+    def __init__(self, client, outfile, output_raw):
         self.client = client
         self.max_json = 100
         self.fd = open(outfile, 'w')
         self.done_msgs = 0
+        self.output_raw = output_raw
 
         # write csv header row
-        self.fd.write("StartDateTime,OpenPrice,HighPrice,LowPrice,LastPrice,Volume,NumTrades,BidVolume,AskVolume\n")
+        if output_raw:
+            self.fd.write("StartDateTime,OpenPrice,HighPrice,LowPrice,LastPrice,Volume,NumTrades,BidVolume,AskVolume\n")
+        else:
+            self.fd.write("DateTime,Price,Volume,AtBidOrAsk\n")
 
     def json_handler(self, msg):
 
@@ -55,24 +58,35 @@ class Downloader:
             print(colored("Unprocess MSG: " + json.dumps(msg), 'red'))
             return
 
-        csv_format = "{StartDateTime},{OpenPrice},{HighPrice},{LowPrice},{LastPrice},{Volume},{NumTrades},{BidVolume},{AskVolume}\n"
-
-        self.fd.write(csv_format.format(
-            StartDateTime = msg["StartDateTime"],
-            OpenPrice = msg["OpenPrice"],
-            HighPrice = msg["HighPrice"],
-            LowPrice = msg["LowPrice"],
-            LastPrice = msg["LastPrice"],
-            Volume = msg["Volume"],
-            NumTrades = msg["NumTrades"],
-            BidVolume = msg["BidVolume"],
-            AskVolume = msg["AskVolume"]
-        ));
+        if self.output_raw:
+            csv_format = "{StartDateTime},{OpenPrice},{HighPrice},{LowPrice},{LastPrice},{Volume},{NumTrades},{BidVolume},{AskVolume}\n"
+            self.fd.write(csv_format.format(
+                StartDateTime = msg["StartDateTime"],
+                OpenPrice = msg["OpenPrice"],
+                HighPrice = msg["HighPrice"],
+                LowPrice = msg["LowPrice"],
+                LastPrice = msg["LastPrice"],
+                Volume = msg["Volume"],
+                NumTrades = msg["NumTrades"],
+                BidVolume = msg["BidVolume"],
+                AskVolume = msg["AskVolume"]
+            ));
+        else:
+            csv_format = "{DateTime},{Price},{Volume},{AtBidOrAsk}\n"
+            #print(msg["BidVolume"], msg["AskVolume"])
+            assert(int(msg["BidVolume"]) == 0 or int(msg["AskVolume"]) == 0)
+            self.fd.write(csv_format.format(
+                DateTime = msg["StartDateTime"],
+                Price = msg["LastPrice"],
+                Volume = msg["Volume"],
+                AtBidOrAsk = '1' if int(msg["BidVolume"]) != 0 else '2'
+            ))
 
         self.done_msgs += 1
 
         if self.done_msgs % 1000000 == 0:
-            print("Has processed %d messages to up %s" % (self.done_msgs, str(datetime.fromtimestamp(msg['StartDateTime'])) if 'StartDateTime' in msg.keys() else "unknown-datetime"))
+            print("Has processed %d messages to up %s" % (
+                self.done_msgs, str(datetime.fromtimestamp(msg['StartDateTime'])) if 'StartDateTime' in msg.keys() else "unknown-datetime"))
 
 def Main():
 
@@ -82,11 +96,14 @@ def Main():
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--userpass', "-i", default="userpass", help="Username and Password file.")
-    parser.add_argument('--address', "-a", default="192.168.130.103", help="IP Address of Sierra Chart instance")
-    parser.add_argument('--port', "-p", type=int, default=8888, help="Port number of Sierra Chart instance")
+    parser.add_argument('--address', "-a", default="192.168.122.107", help="IP Address of Sierra Chart instance")
+    parser.add_argument('--port', "-p", type=int, default=11198, help="Port number of Sierra Chart instance")
     parser.add_argument('--symbol', "-s", required=True, help="Symbol Name")
     parser.add_argument('--exchange', "-e", default="CME", help="Exchange Name")
+    parser.add_argument('--sDateTime', default="0", help="Start DateTime")
+    parser.add_argument('--eDateTime', default="0", help="End DateTime")
     parser.add_argument('--output', "-o", default=None, help="Output file name")
+    parser.add_argument('--raw', default=False, action='store_true', help="output raw file")
 
     args = parser.parse_args()
 
@@ -94,6 +111,8 @@ def Main():
     PORT = args.port
     SYMBOL = args.symbol
     EXCHANGE = args.exchange
+    sDateTime = int(args.sDateTime)
+    eDateTime = int(args.eDateTime)
     OUTPUT = "%s.csv" % SYMBOL if args.output == None else args.output
 
 
@@ -113,14 +132,20 @@ def Main():
         'Symbol': SYMBOL,
         'Exchange': EXCHANGE,
         'RecordInterval': DTC.INTERVAL_TICK,
-        'StartDateTime': 0,
-        'EndDateTime': 0,
+        'StartDateTime': sDateTime,
+        'EndDateTime': eDateTime,
         'MaxDaysToReturn': 0,
         'UseZLibCompression': 0
     })
 
-    d = Downloader(client, OUTPUT)
-    client.run(d.json_handler)
+    d = Downloader(client, OUTPUT, args.raw)
+    try:
+        client.run(d.json_handler)
+    except Exception as err:
+        if err.args[0] == "Done":
+            return
+        else:
+            raise err
 
 if __name__ == "__main__":
     Main()
