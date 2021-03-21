131f60601b7d7cab7900573371f7adf5b915b15d
Junliang Yan <jyan@ca.ibm.ca>
2021-03-21 14:31:52 -0400

    Rename file

--- download_today.sh
+++ download_today.sh
@@ -5,8 +5,8 @@ source env/bin/activate
 
 PYTHON=$(which python3)
 
-ES_SYMBOL=ESH21
-NQ_SYMBOL=NQH21
+ES_SYMBOL=ESM1-CME
+NQ_SYMBOL=NQM1-CME
 
 ES_LOG=$ES_SYMBOL-$DATE.log
 NQ_LOG=$NQ_SYMBOL-$DATE.log
@@ -14,5 +14,5 @@ NQ_LOG=$NQ_SYMBOL-$DATE.log
 FROM=$(date --date='yesterday 17:59:59' +"%s")
 TO=$(date --date='today 17:00:00' +"%s")
 echo "downloading trades from $FROM - $TO"
-$PYTHON HistoricalDataDownloader.py -s $ES_SYMBOL -a 192.168.122.169 --sDateTime=$FROM --eDateTime=$TO -o today.trades
+$PYTHON HistoricalDataDownloader.py -s $ES_SYMBOL -a 192.168.122.142 --sDateTime=$FROM --eDateTime=$TO -o today.tick
 
